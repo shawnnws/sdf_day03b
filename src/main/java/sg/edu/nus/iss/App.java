ce6343bfc30720d4655feda2abb3b62e3834149d
shawnnws <shawnngweisong@gmail.com>
2023-04-19 13:28:05 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                
+            }
+        }
+    }
+}
